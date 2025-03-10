c55396bfbcea359b88dd2ce82c334f51a6160c87
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-03-10 09:09:39 -0300

    Exceptions StandardError, Service ResourceNotFoundException, Resource ResourceExceptionHandler

---
+++ client/src/main/java/com/adriano/client/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.adriano.client.resources.exceptions;
+
+import com.adriano.client.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError();
+
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource not found");
+        err.setMsg(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
